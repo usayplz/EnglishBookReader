5e1e65c06aa4313067f95efa449766fe2f954aac
pandozer <mrpandozer@gmail.com>
2016-03-17 19:46:37 +0700

    search on sdcards

--- app/src/main/java/com/usayplz/englishbookreader/libraly/ScanDriveEngine.java
+++ app/src/main/java/com/usayplz/englishbookreader/libraly/ScanDriveEngine.java
@@ -1,13 +1,19 @@
 package com.usayplz.englishbookreader.libraly;
 
+import android.os.Build;
 import android.os.Environment;
+import android.text.TextUtils;
 
 import com.usayplz.englishbookreader.model.BookType;
 
 import java.io.File;
 import java.io.FileFilter;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 import rx.Observable;
 
@@ -16,15 +22,21 @@ import rx.Observable;
  * u.sayplz@gmail.com
  */
 public class ScanDriveEngine {
+    private static final Pattern DIR_SEPORATOR = Pattern.compile("/");
+
     public ScanDriveEngine() {
     }
 
     public Observable<File> find() {
         return Observable.defer(() -> {
             List<File> files = new ArrayList<>();
-            searchFiles(Environment.getExternalStorageDirectory(), files, f -> {
-                return f.isDirectory() || BookType.byExtension(f.getName()) != null; //TODO: add other formats
-            });
+            for (String dirPath : getStorageDirectories()) {
+                File dir = new File(dirPath);
+                if (!dir.exists()) continue;
+                searchFiles(dir, files, f -> {
+                    return f.isDirectory() || BookType.byExtension(f.getName()) != null;
+                });
+            }
 
             return Observable.from(files);
         });
@@ -38,10 +50,54 @@ public class ScanDriveEngine {
         File[] files = f.listFiles(filter);
 
         for (File file : files) {
-            if (file.isDirectory())
+            if (file.isDirectory()) {
                 searchFiles(file, list, filter);
-            else
+            } else {
                 list.add(file);
+            }
         }
+    }
+
+    public static String[] getStorageDirectories() {
+        final Set<String> rv = new HashSet<>();
+        final String rawExternalStorage = System.getenv("EXTERNAL_STORAGE");
+        final String rawSecondaryStoragesStr = System.getenv("SECONDARY_STORAGE");
+        final String rawEmulatedStorageTarget = System.getenv("EMULATED_STORAGE_TARGET");
+
+        if (TextUtils.isEmpty(rawEmulatedStorageTarget)) {
+            if (TextUtils.isEmpty(rawExternalStorage)) {
+                rv.add("/storage/sdcard0");
+            } else {
+                rv.add(rawExternalStorage);
+            }
+        } else {
+            final String rawUserId;
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                rawUserId = "";
+            } else {
+                final String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+                final String[] folders = DIR_SEPORATOR.split(path);
+                final String lastFolder = folders[folders.length - 1];
+                boolean isDigit = false;
+                try {
+                    Integer.valueOf(lastFolder);
+                    isDigit = true;
+                } catch (NumberFormatException ignored) {
+                }
+                rawUserId = isDigit ? lastFolder : "";
+            }
+            // /storage/emulated/0[1,2,...]
+            if (TextUtils.isEmpty(rawUserId)) {
+                rv.add(rawEmulatedStorageTarget);
+            } else {
+                rv.add(rawEmulatedStorageTarget + File.separator + rawUserId);
+            }
+        }
+
+        if (!TextUtils.isEmpty(rawSecondaryStoragesStr)) {
+            final String[] rawSecondaryStorages = rawSecondaryStoragesStr.split(File.pathSeparator);
+            Collections.addAll(rv, rawSecondaryStorages);
+        }
+        return rv.toArray(new String[rv.size()]);
     }
 }
